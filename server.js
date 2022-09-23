96e5032a083cc2b087145f5a73613fb8b660168f
MDBelfast <gzxc3kc4690@gmail.com>
2022-09-24 00:05:32 +0800

    Add ip addr for server
    Previously, I didn't give an exact ip address when create server.
    When creating by docker, the browser cannot connect to the server.
    After an exact ip address is added, the browser can connect to it.

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.put('/putUser/:Id', function (req, res) {
    res.end( JSON.stringify(req.body));
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(8081, '0.0.0.0', function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
