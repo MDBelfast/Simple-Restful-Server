d7fd5cbc05e8fbb7509941e2e65a34e580b2b139
MDBelfast <gzxc3kc4690@gmail.com>
2022-10-20 03:44:48 +0800

    Add unit-test for db init
    A file init_data.txt is added for checking the initialization of
    users table in the Postgres database after running the container in
    docker.

--- psql_test.sh
+++ psql_test.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/bash
 
-docker exec -it $(docker ps -aqf "name=simple_restful-db-1") psql -U postgres -c "select * from users;"
+docker exec -it $(docker ps -aqf "name=simple_restful-db-1") psql -U postgres -c "select * from users;" 1> db_init_test.txt
+sed -i -e "s/\r//g" db_init_test.txt
+
+diff init_data.txt db_init_test.txt > /dev/null 2>&1
+error=$?
+if [ $error -eq 0 ]
+then
+   echo "init_data.txt and db_init_test.txt are the same file"
+elif [ $error -eq 1 ]
+then
+   echo "init_data.txt and db_init_test.txt differ"
+else
+   echo "There was something wrong with the diff command"
+fi
 
