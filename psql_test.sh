02b46e3c86429fb1cfce21ba8ab90f7f004186a1
MDBelfast <gzxc3kc4690@gmail.com>
2022-11-03 23:24:42 +0800

    Use docker-compose instead of docker

--- psql_test.sh
+++ psql_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-docker exec -T $(docker ps -aqf "name=postgres_db") psql -U postgres -c "select * from users;" 1> db_init_test.txt
+docker-compose exec -T $(docker ps -aqf "name=postgres_db") psql -U postgres -c "select * from users;" 1> db_init_test.txt
 sed -i -e "s/\r//g" db_init_test.txt
 
 diff init_data.txt db_init_test.txt > /dev/null 2>&1
