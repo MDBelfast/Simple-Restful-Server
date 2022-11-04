d2a2e69f43338b7351e2d036c9873fefe00d4f7d
MDBelfast <gzxc3kc4690@gmail.com>
2022-11-04 23:49:29 +0800

    Add the second docker ps
    Add the second docker ps to check existing of containers.

--- psql_test.sh
+++ psql_test.sh
@@ -3,6 +3,7 @@
 docker ps
 docker-compose exec -T $(docker ps -aqf "name=postgres_db") psql -U postgres -c "select * from users;" 1> db_init_test.txt
 sed -i -e "s/\r//g" db_init_test.txt
+docker ps
 
 diff init_data.txt db_init_test.txt > /dev/null 2>&1
 error=$?
