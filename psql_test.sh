82e3a527090d474bc31fe3809b55ffccbcdc1058
MDBelfast <gzxc3kc4690@gmail.com>
2022-11-09 21:34:42 +0800

    Remove display for debug

--- psql_test.sh
+++ psql_test.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 
-docker ps
 docker exec postgres_db psql -U postgres -c "select * from users;" 1> db_init_test.txt
 sed -i -e "s/\r//g" db_init_test.txt
-docker-compose ps
 
 diff init_data.txt db_init_test.txt > /dev/null 2>&1
 error=$?
